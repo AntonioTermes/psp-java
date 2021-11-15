aac1b32a83e21a8794ad7ede293e86a37a1adb01
ATG <200104@iessierradeguara.com>
2021-11-15 10:38:01 +0100

    Añadido Ejecutor

--- hilos02-ContadorPalabras/src/ContadorPalabras.java
+++ hilos02-ContadorPalabras/src/ContadorPalabras.java
@@ -4,24 +4,32 @@ import java.util.Scanner;
 
 public class ContadorPalabras {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 
 		String rutaFichero = "/home/alumno/texto.txt";
-
-		if (args.length > 0) {
+		if (args.length != 0) {
+			
 			rutaFichero = args[0];
+			
 		}
 
-		Scanner lector = new Scanner(new File(rutaFichero));
-		int contador = 0;
-
-		while (lector.hasNext()) {
-			lector.next();
-			contador++;
-		}
-
+		Scanner lector;
+		try {
+			
+			lector = new Scanner(new File(rutaFichero));
+			int contador = 0;
+			while (lector.hasNext()) {
+				lector.next();
+				contador++;
+				
+			}
+			
 		System.out.println("Total palabras: " + contador + ".");
-
+			
+		} catch (FileNotFoundException e) {
+			
+			System.out.println("Fichero " + rutaFichero + " no encontrado.");
+			
+		}
 	}
-
 }
---
+++ hilos02-ContadorPalabras/src/Ejecutor.java
@@ -0,0 +1,11 @@
+
+public class Ejecutor {
+	
+	public static void main(String [] args) {
+		
+		String [] vectorVacio = {};
+		ContadorPalabras.main(vectorVacio);
+		
+	}
+
+}
